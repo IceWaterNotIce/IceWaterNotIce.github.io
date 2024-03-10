cc4e532cdad4038d883f68a27d3330d664fe6666
冰水不冰 <icewaternotice@gmail.com>
2024-03-10 20:45:08 +0800

    Refactor code to improve performance and readability

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  // Your code here
+  let module = import.meta.glob('/assets/images/comfyUI/*.png');
+  console.log(module);
+  return module;
+});
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <img alt="Vue logo" src="/assets/images/comfyUI/ComfyUI_00001_.png" />
+  </div>
+</template>
