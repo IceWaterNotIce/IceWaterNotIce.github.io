414e7ac88005f0e8b0e51a7e4d830f9f95ba6174
冰水不冰 <icewaternotice@gmail.com>
2024-04-20 19:53:59 +0800

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-  base: '/icewaternotice.github.io/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
