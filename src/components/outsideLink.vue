accc50c7970179368fb3d7f03b181e4fa1c62359
IceWaterNotIce <icewaternotice@gmail.com>
2024-08-28 08:25:06 +0800

    added background for outsideLink icon

--- src/components/outsideLink.vue
+++ src/components/outsideLink.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="outsideLink">
     <!-- Link to bilibili-->
-    <a href="https://space.bilibili.com/3494373465524892?spm_id_from=333.1007.0.0">
-      <img src="\public\images\bilibili-brands-solid.svg" alt="bilibili" width="30" height="30" />
-    </a>
+    <div class="bilibili">
+      <a href="https://space.bilibili.com/3494373465524892?spm_id_from=333.1007.0.0">
+        <img src="\public\images\bilibili-brands-solid.svg" alt="bilibili" width="30" height="30" />
+      </a>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss">
-.outsideLink{
-    // locate it at the bottom left corner of the page
-    position:absolute;
-    left:0;
-    bottom: 0;
-    // padding
-    padding-left: 5%;
-    padding-bottom: 5%;
+.outsideLink {
+  // locate it at the bottom left corner of the page
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  // padding
+  padding-left: 1%;
+  padding-bottom: 1%;
+}
+.bilibili {
+  // circle background
+  width: 40px;
+  height: 40px;
+  background-color: #fff;
+  border-radius: 50%;
+  // center the icon
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
