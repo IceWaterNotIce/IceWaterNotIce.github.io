3815a0a28c000e89358c692157d7aceaf3646b3b
冰水不冰 <icewaternotice@gmail.com>
2024-09-01 16:51:41 +0800

    change to ts and correct circles moving

--- src/components/animated-circles-background.vue
+++ src/components/animated-circles-background.vue
@@ -4,7 +4,9 @@
 
 <style lang="scss">
 #animated-circles-background {
+  // make the canvas under the content
   z-index: -1;
+  // make the canvas full of the parent element
   position: absolute;
   width: 100%;
   height: 100%;
@@ -13,27 +15,43 @@
 }
 </style>
 
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue'
 
 onMounted(() => {
   // Get the canvas element
-  var canvas = document.getElementById('animated-circles-background')
+  var canvas = document.getElementById('animated-circles-background') as HTMLCanvasElement
+  var ctx = canvas.getContext('2d')
 
   // get parent element's width and height
   var parent = canvas.parentElement
-  canvas.width = parent.clientWidth
-  canvas.height = parent.clientHeight
-
-  var ctx = canvas.getContext('2d')
+  canvas.width = parent ? parent.clientWidth : 0
+  canvas.height = parent ? parent.clientHeight : 0
+  let resizeTimeout: number | null = null
+  window.addEventListener('resize', function () {
+    if (resizeTimeout) {
+      clearTimeout(resizeTimeout)
+    }
+    resizeTimeout = setTimeout(function () {
+      canvas.width = parent ? parent.clientWidth : 0
+      canvas.height = parent ? parent.clientHeight : 0
+    }, 50)
+  })
 
   // 50 random circles
-  var circles = []
+  var circles: {
+    x: number
+    y: number
+    speed: number
+    direction: number
+    radius: number
+    color: string
+  }[] = []
   for (var i = 0; i < 100; i++) {
     circles.push({
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
-      speed: (Math.random() * 1000) / 10000 + 1,
+      speed: Math.random() + 0.1,
       direction: Math.random() * Math.PI * 2,
       radius: 2,
       color: 'rgba(255, 255, 255, 0.5)'
@@ -44,26 +62,53 @@ onMounted(() => {
   function drawCircle() {
     for (var i = 0; i < circles.length; i++) {
       var circle = circles[i]
-      ctx.beginPath()
-      ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2)
-      ctx.fillStyle = circle.color
-      ctx.fill()
+      if (ctx) {
+        ctx.beginPath()
+        ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2)
+        ctx.fillStyle = circle.color
+        ctx.fill()
+      }
     }
   }
 
   // update the circle's position
   // if it hits the wall, reverse its direction
+  var mouse = { x: -100, y: -100 }
+  window.addEventListener('mousemove', function (e) {
+    mouse.x = e.clientX - canvas.getBoundingClientRect().left
+    mouse.y = e.clientY - canvas.getBoundingClientRect().top
+  })
   function update() {
     for (var i = 0; i < circles.length; i++) {
       var circle = circles[i]
       circle.x += Math.cos(circle.direction) * circle.speed
       circle.y += Math.sin(circle.direction) * circle.speed
 
-      if (circle.x < 0 || circle.x > canvas.width) {
-        circle.direction = Math.PI - circle.direction
+      // if cirle near the mouse, 
+      var dx = circle.x - mouse.x
+      var dy = circle.y - mouse.y
+      var distance = Math.sqrt(dx * dx + dy * dy)
+      if (distance < 100 && circle.x > 0 && circle.x < canvas.width && circle.y > 0 && circle.y < canvas.height) {
+        if (circle.x < mouse.x) {
+          circle.x -= 1
+        } else {
+          circle.x += 1
+        }
+        if (circle.y < mouse.y) {
+          circle.y -= 1
+        } else {
+          circle.y += 1
+        }
       }
-      if (circle.y < 0 || circle.y > canvas.height) {
+
+      // if circle hits the wall, reverse its direction
+      if (circle.x <= 0 || circle.x > canvas.width) {
+        circle.direction = Math.PI - circle.direction
+        circle.speed = Math.random() + 0.1
+      }
+      if (circle.y <= 0 || circle.y > canvas.height) {
         circle.direction = -circle.direction
+        circle.speed = Math.random() + 0.1
       }
     }
   }
@@ -78,35 +123,23 @@ onMounted(() => {
         var dy = circle1.y - circle2.y
         var distance = Math.sqrt(dx * dx + dy * dy)
         if (distance < 100) {
-          ctx.beginPath()
-          ctx.moveTo(circle1.x, circle1.y)
-          ctx.lineTo(circle2.x, circle2.y)
-          ctx.strokeStyle = 'rgba(255, 255, 255, ' + (1 - distance / 100) + ')'
-          ctx.stroke()
+          if (ctx) {
+            ctx.beginPath()
+            ctx.moveTo(circle1.x, circle1.y)
+            ctx.lineTo(circle2.x, circle2.y)
+            ctx.strokeStyle = 'rgba(255, 255, 255, ' + (1 - distance / 100) + ')'
+            ctx.stroke()
+          }
         }
       }
     }
   }
 
-  // if circle near mouse, let it move towards the mouse
-  canvas.addEventListener('mousemove', function (e) {
-    var x = e.clientX - canvas.getBoundingClientRect().left
-    var y = e.clientY - canvas.getBoundingClientRect().top
-    for (var i = 0; i < circles.length; i++) {
-      var circle = circles[i]
-      var dx = x - circle.x
-      var dy = y - circle.y
-      var distance = Math.sqrt(dx * dx + dy * dy)
-      if (distance < 50 && distance > 10) {
-        circle.direction = Math.atan2(dy, dx)
-        circle.speed = distance / 50
-      }
-    }
-  })
-
   // clear the canvas
   function clear() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if (ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+    }
   }
 
   // draw the circle
